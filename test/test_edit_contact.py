6b63a81bea79e9eccdd846f2f7a84af36920ab7e
Lenchik13 <alta00@mail.ru>
2016-04-07 17:04:21 +0300

    #11 Исправлен тест модификации контакта

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -12,12 +12,12 @@ def test_edit_contact(app):
                               notes="", bday="20", bmonth="6"))
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
-    contact = Contact(lastname="Sycheva", firstname="Elena")
     contact.id = old_contacts[index].id
-    app.contact.edit_contact_by_index(index)
     app.open_home_page()
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
 
