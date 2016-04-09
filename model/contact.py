2e70689cce50ac58462aa36807e494a5bf502c45
Lenchik13 <alta00@mail.ru>
2016-04-10 01:14:32 +0300

    L5

--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,7 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, nickname=None, address=None, company=None, home=None, mobile=None, work=None, fax=None, email=None,
+    def __init__(self, firstname=None, lastname=None, all_phones_from_home_page=None, nickname=None, address=None, company=None, home=None, mobile=None, work=None, fax=None, email=None,
                        email2=None, email3=None, homepage=None, byear=None, address2=None, phone2=None, notes=None, bday=None, bmonth=None, id=None):
         self.firstname = firstname
         self.lastname = lastname
@@ -24,6 +24,7 @@ class Contact:
         self.bday = bday
         self.bmonth = bmonth
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
