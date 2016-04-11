f09241714f1d7c9675a580fa3243b3863ce89bfd
Lenchik13 <alta00@mail.ru>
2016-04-11 03:16:39 +0300

    #15 Параметризованы тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -16,7 +16,10 @@ testdata = [Contact(firstname="", lastname="", nickname="", address="", company=
                     email="", email2="", email3="", homepage="", address2="", phone2="", notes="", byear="")] + [
     Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 20), nickname=random_string("nickname", 20),
             address=random_string("address", 20),
+            company=random_string("company", 10), home=random_digits("h", 10), mobile=random_digits("m", 10),
+            work=random_digits("w", 10), fax=random_digits("f", 10), email=random_string("email", 20),
             email2=random_string("email2", 10), email3=random_string("email3", 20), homepage=random_string("homepage", 20),
+            address2=random_string("address2", 20), phone2=random_digits("p2", 11),
             notes=random_string("notes", 10), byear=random_digits("y", 4))
     for i in range (5)
     ]
