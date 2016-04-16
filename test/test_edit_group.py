c807b4373e5be847b741f28ca12dba0bd1b175db
Lenchik13 <alta00@mail.ru>
2016-04-17 01:22:52 +0300

    #19 Реализована загрузка тестовых данных из файла

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,14 +1,6 @@
 from model.group import Group
 from random import randrange
 
-#def test_modify_group(app):
-#    if app.group.count() == 0:
-#        app.group.create(Group(name="gr1", header="grr1", footer="grrr1"))
-#    old_groups = app.group.get_group_list()
-#    app.group.modify_group()
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) == len(new_groups)
-
 
 def test_modify_first_group_name(app):
     if app.group.count() == 0:
@@ -23,12 +15,3 @@ def test_modify_first_group_name(app):
     old_groups[index] = group
     print (sorted(old_groups, key=Group.id_or_max))
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-#def test_modify_first_group_header(app):
-#    if app.group.count() == 0:
-#    app.group.create(Group(name="gr1", header="grr1", footer="grrr1"))
-#    old_groups = app.group.get_group_list()
-#    app.group.modify_first_group(Group(header="New header"))
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) == len(new_groups)
